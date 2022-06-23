0fe7e33e407d39be68af620480174566adecf6c2
Sean Gillis <seangillis@Seans-MBP.attlocal.net>
2022-06-22 19:40:26 -0500

    no major updates

--- controllers/trivia.js
+++ controllers/trivia.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const postgres = require('../postgres.js');
 
+const randomid = () => {
+    
+}
+
 router.get('/', (req, res) => {
     postgres.query('SELECT * FROM questions ORDER BY id ASC;', (err, results) => {
         res.json(results.rows)
